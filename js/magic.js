73b90cbd6226ffc5ba260c1b38e20db1583b9112
rqsell <68514875+rqsell@users.noreply.github.com>
2020-08-26 17:58:51 -0400

    Wednesday version

--- js/magic.js
+++ js/magic.js
@@ -6,3 +6,16 @@ function getImage() {
     
 } 
 
+var request = require('superagent');
+
+var clientID = '1eba11dd5728224e4393',
+    clientSecret = 'c89b30364999a2ed914e76f8a195fa76',
+    apiUrl = 'https://api.artsy.net/api/tokens/xapp_token',
+    xappToken;
+
+request
+  .post(apiUrl)
+  .send({ client_id: clientID, client_secret: clientSecret })
+  .end(function(res) {
+    xappToken = res.body.token; 
+  });
